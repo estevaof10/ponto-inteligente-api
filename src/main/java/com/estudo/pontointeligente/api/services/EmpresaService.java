0c06a79a3d02a1138863679422e9ee9b091c330f
Estêvão Filipe <estevaof10@gmail.com>
2021-02-02 21:53:53 -0300

    Criando Services

---
+++ src/main/java/com/estudo/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,12 @@
+package com.estudo.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.estudo.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaService {
+	
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+	
+	Empresa persistir(Empresa empresa);
+}
